54a95779af6c436bfcafc84dfdabce2e7c52dd1e
hsdistefa <hsdistefa@gmail.com>
2024-10-28 19:32:01 -0700

    Triangle calculation

---
+++ WebApplication1/Program.cs
@@ -0,0 +1,11 @@
+using System;
+class GlazerCalc
+{
+    static void Main()
+    {
+       float result;
+       float b = 1.5f;
+       float h = 4f;
+       Console.WriteLine(0.5 * b * h); 
+    }
+}
